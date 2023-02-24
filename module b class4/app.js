b3c500248628b887817d9e16436fbc6649e4c6af
SaadKhan6969 <115664292+SaadKhan6969@users.noreply.github.com>
2023-02-24 19:48:00 +0500

    Add files via upload

---
+++ module b class4/app.js
@@ -0,0 +1,99 @@
+//  ========  Map  ===========
+
+
+// const arr =[1,2,3,4,5,6]
+
+// arr.map(function(a,b){
+// console.log('value====>',a)
+// const array =a*2
+// console.log(array)
+// })
+
+
+
+
+// const newarray=arr.map(function(a,b,c){
+
+
+//     return a*2
+// })
+// console.log(newarray)
+// console.log(arr)
+
+// ===== functions =====
+
+// ====highorder functions====
+//  callback funtion
+
+
+
+// function abc(a){
+// console.log(a)
+// }
+
+// function xyz(x){
+//     return x
+// }
+
+// abc(xyz('saadd'))
+
+
+
+// Function called In an Other Ftn IS called callback ftn
+
+// ftn thAT CALLS Another ftn in Its Parameter Is Called high order ftns
+
+
+// function abc(a){
+//     console.log(a)
+//     return function(b){
+//         console.log(b)
+//     }
+// }
+
+// let a=abc('ABC')
+// a('SAaad')
+
+
+// function abc(a){
+//     console.log(a)
+//     return function(b){
+//         console.log(b)
+//     }
+// }
+
+// abc("ABC")("XYZ")
+
+// let a=function(){
+//     console.log("abc")
+// }
+// a()
+
+// let b= (a) => {
+//     console.log(a)
+// }
+// b("abccc")
+
+// let b= () => {
+//     console.log("abv")
+// }
+// b()
+
+
+// let a =(()=>{
+//     return "ABC"
+// })()
+
+
+// console.log(a)
+
+// let a =()=>{
+//     return "ABC"
+// }
+
+
+// console.log(a())
+
+
+let c =(undefined || 0) * 1 - 10
+console.log(c)
